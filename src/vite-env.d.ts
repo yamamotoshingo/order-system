9e0831f929707c1e29b54dc64dc93029ef22bd76
yamamotoshingo <thgy3r28sd@outlook.jp>
2025-11-06 18:55:39 +0900

    20251106_試し１

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
-/// <reference types="vite/client" />
+// /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_LIFF_ID: string;
-}
+// interface ImportMetaEnv {
+//   readonly VITE_LIFF_ID: string;
+// }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+// interface ImportMeta {
+//   readonly env: ImportMetaEnv;
+// }
